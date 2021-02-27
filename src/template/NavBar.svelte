7a7f7ea05909ff6f92f096ea80b2ef864d3cfc9e
dongzoolee <nant0313@gmail.com>
2021-02-28 00:47:41 +0900

    Create NavBar Component

---
+++ src/template/NavBar.svelte
@@ -0,0 +1,23 @@
+<script>
+    import './NavBar.scss';
+</script>
+
+<div class="navbar">
+    <div class="navbar_logo_container">
+        <a href="/">
+            <img class="navbar_logo" src="/res/logo-crimson.svg" alt="Sogang ACM-ICPC Team">
+        </a>
+    </div>
+    <div class="navbar_nav_container">
+        <ul>
+            <li class="nav_item"><a href="/introduction">소개</a></li>
+            <li class="nav_item"><a href="/history">기록</a></li>
+            <li class="nav_item"><a href="/spc">SPC</a></li>
+            <li class="nav_item"><a href="/contact">가입 및 문의</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+
+</style>
