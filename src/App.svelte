ff66208bd85cbb292dc724d44ec17446f69025e2
dongzoolee <nant0313@gmail.com>
2021-03-01 01:02:48 +0900

    Add Routers

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,28 @@
 <script>
+	import router from 'page';
 	import NavBar from './template/NavBar/NavBar.svelte';
 	import Footer from './template/Footer/Footer.svelte';
+	import Home from './routes/Home.svelte';
+	import History from './routes/History/History.svelte';
+	import Spc from './routes/Spc/Spc.svelte';
+	import Contact from './routes/Contact/Contact.svelte';
+	import Introduction from './routes/Introduction/Introduction.svelte';
+
+	let page;
+	router('/', () => (page = Home));
+	router('/introduction', () => (page = Introduction));
+	router('/history', () => (page = History));
+	router('/spc', () => (page = Spc));
+	router('/contact', () => (page = Contact));
+
+	router.start();
 </script>
 
-<NavBar />
-<Footer />
+<div class="contents_container">
+	<NavBar />
+	<svelte:component this={page} />
+	<Footer />
+</div>
+
 <style>
-	
 </style>
