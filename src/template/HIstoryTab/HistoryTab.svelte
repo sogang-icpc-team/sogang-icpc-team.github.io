7c22240c446b3753a03c780405c8f61268a0e707
dongzoolee <nant0313@gmail.com>
2021-03-01 08:48:21 +0900

    Create HistoryTab Component

---
+++ src/template/HIstoryTab/HistoryTab.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let yearsList;
+
+	const switchSelected = (e) => {
+		let ul = e.target.closest('li').parentElement;
+
+		ul.querySelectorAll('li').forEach((ele) => {
+            if(ele !== e.target)
+			    ele.classList.remove('selected');
+		});
+        e.target.closest('li').classList.add('selected')
+	};
+</script>
+
+{#if yearsList}
+	{#each yearsList as data, idx}
+		<li class={idx == 0 ? 'selected' : ''} on:click={switchSelected}>
+			<span class="tab_text">{data}</span>
+		</li>
+	{/each}
+{/if}
