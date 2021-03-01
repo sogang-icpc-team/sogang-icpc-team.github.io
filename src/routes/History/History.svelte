97486534b87bad07474431d80ca1317b8f32dcc6
dongzoolee <nant0313@gmail.com>
2021-03-01 09:52:07 +0900

    Import OrganizerTable Component

--- src/routes/History/History.svelte
+++ src/routes/History/History.svelte
@@ -1,6 +1,7 @@
 <script>
 	import HistoryTable from '../../template/HistoryTable/HistoryTable.svelte';
 	import HistoryTab from '../../template/HistoryTab/HistoryTab.svelte';
+	import OrganizerTable from '../../template/OrganizerTable/OrganizerTable.svelte';
 	let yearsList,
 		history,
 		curYear = 2020;
@@ -70,143 +71,7 @@
 	</div>
 	<div class="p75">
 		<div class="table_container">
-			<table style="min-width: 800px">
-				<tr>
-					<th>연도</th>
-					<th>이름</th>
-					<th>직책</th>
-					<th>BOJ 핸들</th>
-					<th>비고</th>
-				</tr>
-				<!-- 2021 -->
-				<tr>
-					<td rowspan="2"><b>2021 - 현재</b></td>
-					<td>이민희</td>
-					<td>학회장</td>
-					<td><a href="https://www.acmicpc.net/user/minigimbob" target="_blank">minigimbob</a></td>
-					<td>minigimbob@gmail.com</td>
-				</tr>
-				<tr>
-					<td>이동주</td>
-					<td>부학회장</td>
-					<td><a href="https://www.acmicpc.net/user/k512" target="_blank">k512</a></td>
-					<td><a href="https://github.com/nant0313">github.com/nant0313</a></td>
-				</tr>
-				<!-- 2020 -->
-				<tr>
-					<td rowspan="2">2020</td>
-					<td>임지환</td>
-					<td>학회장</td>
-					<td><a href="https://www.acmicpc.net/user/raararaara" target="_blank">raararaara</a></td>
-					<td>raararaara@gmail.com</td>
-				</tr>
-				<tr>
-					<td>김진수</td>
-					<td>부학회장</td>
-					<td><a href="https://www.acmicpc.net/user/fakingate" target="_blank">fakingate</a></td>
-					<td>fakingate@gmail.com</td>
-				</tr>
-				<!-- 2019 -->
-				<tr>
-					<td rowspan="2">2019</td>
-					<td>박수현</td>
-					<td>학회장</td>
-					<td><a href="https://www.acmicpc.net/user/shiftpsh" target="_blank">shiftpsh</a></td>
-					<td><a href="https://shiftpsh.com" target="_blank">shiftpsh.com</a></td>
-				</tr>
-				<tr>
-					<td>이태한</td>
-					<td>부학회장</td>
-					<td><a href="https://www.acmicpc.net/user/sogangcse" target="_blank">sogangcse</a></td>
-					<td />
-				</tr>
-				<!-- 2018 -->
-				<tr>
-					<td rowspan="2">2018</td>
-					<td>최윤영</td>
-					<td>학회장</td>
-					<td><a href="https://www.acmicpc.net/user/ainch96" target="_blank">ainch96</a></td>
-					<td />
-				</tr>
-				<tr>
-					<td>김인호</td>
-					<td>부학회장</td>
-					<td />
-					<td />
-				</tr>
-				<!-- 2017 -->
-				<tr>
-					<td rowspan="2">2017</td>
-					<td>최윤영</td>
-					<td>학회장</td>
-					<td><a href="https://www.acmicpc.net/user/ainch96" target="_blank">ainch96</a></td>
-					<td />
-				</tr>
-				<tr>
-					<td>정성엽</td>
-					<td>부학회장</td>
-					<td><a href="https://www.acmicpc.net/user/scka" target="_blank">scka</a></td>
-					<td />
-				</tr>
-				<!-- 2016 -->
-				<tr>
-					<td rowspan="2">2016</td>
-					<td>김도현</td>
-					<td>학회장</td>
-					<td />
-					<td><a href="https://ddyokim.github.io/" target="_blank">ddyokim.github.io</a></td>
-				</tr>
-				<tr>
-					<td>최윤영</td>
-					<td>부학회장</td>
-					<td><a href="https://www.acmicpc.net/user/ainch96" target="_blank">ainch96</a></td>
-					<td />
-				</tr>
-				<!-- 2015 -->
-				<tr>
-					<td rowspan="2">2015</td>
-					<td>김선영</td>
-					<td>학회장</td>
-					<td />
-					<td
-						><a href="https://www.linkedin.com/in/seon-young-kim-38a058a0/?locale=en_US" target="_blank"
-							>LinkedIn</a
-						></td
-					>
-				</tr>
-				<tr>
-					<td>김도현</td>
-					<td>부학회장</td>
-					<td />
-					<td><a href="https://ddyokim.github.io/" target="_blank">ddyokim.github.io</a></td>
-				</tr>
-				<!-- 2014 -->
-				<tr>
-					<td>2014</td>
-					<td>고창영</td>
-					<td>학회장</td>
-					<td><a href="https://www.acmicpc.net/user/WeissBlume" target="_blank">WeissBlume</a></td>
-					<td><a href="https://lucent.me/" target="_blank">lucent.me</a></td>
-				</tr>
-				<!-- 2013 -->
-				<!-- 2012 -->
-				<tr>
-					<td>2012&mdash;2013</td>
-					<td>김상근</td>
-					<td>학회장</td>
-					<td><a href="https://www.acmicpc.net/user/hihihi" target="_blank">hihihi</a></td>
-					<td><a href="https://sisobus.com/" target="_blank">sisobus.com</a></td>
-				</tr>
-				<!-- 2011 -->
-				<!-- 2007 -->
-				<tr>
-					<td>2007&mdash;2011</td>
-					<td>최백준</td>
-					<td>학회장</td>
-					<td><a href="https://www.acmicpc.net/user/baekjoon" target="_blank">baekjoon</a></td>
-					<td><a href="https://baekjoon.com/" target="_blank">baekjoon.com</a></td>
-				</tr>
-			</table>
+			<OrganizerTable />
 		</div>
 
 		<p style="margin-top: 16px;">
