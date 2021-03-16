53bae847327c12b1c52b53ceb9b54a1df795ff82
dongzoolee <nant0313@gmail.com>
2021-03-17 02:15:35 +0900

    Create ReviewTable Component

---
+++ src/template/ReviewTable/ReviewTable.svelte
@@ -0,0 +1,37 @@
+<script>
+	export let title, tbody;
+</script>
+
+<div class={`row pad review_table hide`}>
+	<div class="p25">
+		<h2>{title}</h2>
+	</div>
+	<div class="p75">
+		<div class="table_container">
+			<table>
+				<thead
+					><tr>
+						<th>링크</th>
+					</tr></thead
+				>
+				<tbody>
+					{#each tbody as data, idx1}
+						<tr>
+							<td><a href={data[1]}>{data[0]}</a></td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
+		<p />
+	</div>
+</div>
+
+<style lang="scss">
+	.hide {
+		display: none;
+	}
+	.show {
+		display: block;
+	}
+</style>
