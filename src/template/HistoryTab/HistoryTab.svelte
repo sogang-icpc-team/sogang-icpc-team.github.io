ae349cdea355594b2f18d66e640e7d24cc02a305
nant0313 <nant0313@gmail.com>
2021-03-27 16:12:24 +0900

    Add HistoryTab Component

---
+++ src/template/HistoryTab/HistoryTab.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let yearsList, curYear;
+
+	const switchSelected = (e) => {
+        // Change curYear Value To Re-fetch Json
+        curYear = e.target.textContent
+        
+        // Toggling Class
+		let ul = e.target.closest('li').parentElement;
+		ul.querySelectorAll('li').forEach((ele) => {
+            if(ele !== e.target)
+			    ele.classList.remove('selected');
+		});
+        e.target.closest('li').classList.add('selected')
+	};
+</script>
+
+{#if yearsList}
+	{#each yearsList as data, idx}
+		<li class={idx == 0 ? 'selected' : ''} on:click={switchSelected}>
+			<span class="tab_text">{data}</span>
+		</li>
+	{/each}
+{/if}
