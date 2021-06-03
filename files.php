76335797f708966791222c3f58e661be3fad0d00
nant0313 <nant0313@users.noreply.github.com>
2021-06-03 07:30:11 +0000

    deploy: 11e1de14b25182733bc4eb2452210aa481d9c9c9

---
+++ files.php
@@ -0,0 +1,5 @@
+<?php
+$pictures = [
+    ["acm-icpc2018.jpg", "ACM-ICPC Asia Seoul Regional Constes 2018 - Tean no team name"],
+    ["icpc2019-redshift.jpg", "ICPC Asia Seoul Regional Constes 2019 - Tean Redshift"]
+];
