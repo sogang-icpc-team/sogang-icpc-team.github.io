6c134f81ca6b47ae63a123366fc5f91a5234b2e2
dongzoolee <nant0313@gmail.com>
2021-06-03 16:25:01 +0900

    Create add-routers-html.sh

---
+++ add-routers-html.sh
@@ -0,0 +1,6 @@
+for filename in src/routes/*; do
+  if [[ -d "$filename" ]]; then
+    lower_filename=$(echo "$filename".html | tr '[:upper]' '[:lower]')
+    cp public/index.html public/"$lower_filename"
+  fi
+done
