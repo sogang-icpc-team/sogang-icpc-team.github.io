65687ad6e2790746b046a681af22bd6ba194a802
Suhyun Park <me@shiftpsh.com>
2023-05-01 16:59:20 +0900

    Create apply-cleanwater.svelte

---
+++ src/routes/apply-cleanwater.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { onMount } from 'svelte';
+	onMount(() => {
+		window.location.replace('https://docs.google.com/forms/d/1EMXoLTldWQjsjSvc1KFfLuQJgfYqk0pwcxHoULJTk24');
+	});
+</script>
+
+<style>
+</style>
