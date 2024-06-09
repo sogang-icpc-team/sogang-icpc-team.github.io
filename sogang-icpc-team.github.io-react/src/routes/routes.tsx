854cd410675b03c5728177a1a7c892fa42160efe
dongzoolee <me@leed.at>
2024-06-09 08:31:43 +0000

    Improve stuctures of routes.tsx

--- sogang-icpc-team.github.io-react/src/routes/routes.tsx
+++ sogang-icpc-team.github.io-react/src/routes/routes.tsx
@@ -1,12 +1,63 @@
+import _ from "lodash";
+
 import { MainPage } from "../app/main-page/main-page";
 
-type TRoute = {
-  path: string;
-  component: React.ReactNode;
-};
-export const routes: TRoute[] = [
-  {
-    path: "/",
+const _routes = {
+  "/": {
     component: <MainPage />,
   },
-];
+  main: {
+    component: <MainPage />,
+  },
+  introduction: {
+    component: <></>,
+  },
+  history: {
+    component: <></>,
+  },
+  spc: {
+    component: <></>,
+  },
+  applyK512: {
+    component: <></>,
+  },
+  contact: {
+    component: <></>,
+  },
+} as const;
+
+export const buildRoutesArray = (routes: {
+  [k: string]: {
+    component: React.ReactNode;
+  };
+}): {
+  path: string;
+  component: React.ReactNode;
+}[] => {
+  return Object.entries(_routes).map(([route, { component }]) => {
+    const kebabPath = _.kebabCase(route);
+    return {
+      path: kebabPath.startsWith("/") ? kebabPath : `/${kebabPath}`,
+      component,
+    };
+  });
+};
+
+export const routes = Object.entries(_routes).reduce(
+  (acc, [route, { component }]) => {
+    const kebabPath = _.kebabCase(route);
+    return {
+      [route]: {
+        component,
+        path: () => (kebabPath.startsWith("/") ? kebabPath : `/${kebabPath}`),
+      },
+      ...acc,
+    };
+  },
+  {},
+) as {
+  [k in keyof typeof _routes]: {
+    component: React.ReactNode;
+    path: () => string;
+  };
+};
