d72deaa964ee3b6ab470bd42d353aaae4ee97847
dongzoolee <me@leed.at>
2024-06-09 08:28:25 +0000

    Polish MainPage

--- sogang-icpc-team.github.io-react/src/common/themes/theme.ts
+++ sogang-icpc-team.github.io-react/src/common/themes/theme.ts
@@ -1,3 +1,25 @@
+import { css } from "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme extends TSgIcpcTheme {}
+}
+
 export const theme = {
-  breakpoints: {},
-};
+  color: {
+    primary: "#b60005",
+  },
+  zIndex: {
+    navigationBar: 2_000,
+  },
+  page: { backgroundColor: "#ffffff" },
+  breakpoints: {
+    mobile: "@media only screen and (max-width: 768px)",
+  },
+  nonSelectableStyle: css`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `,
+} as const;
+
+type TSgIcpcTheme = typeof theme;
