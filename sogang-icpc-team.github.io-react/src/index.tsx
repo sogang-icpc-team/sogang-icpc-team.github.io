33d4acadb7985e726c748773b9bfdb9c31bfadfd
dongzoolee <me@leed.at>
2024-06-10 06:12:34 +0000

    Introduce OrganizerDataContext

--- sogang-icpc-team.github.io-react/src/index.tsx
+++ sogang-icpc-team.github.io-react/src/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
-import { buildRoutes, routes } from "./routes/routes";
+import { OrganizerDataContextProvider } from "./contexts/organizer-data-context";
+import { buildRoutesArray, routes } from "./routes/routes";
 import { theme } from "./common/themes/theme";
 
 import "./common/themes/global.scss";
@@ -12,13 +12,15 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <Routes>
-        {buildRoutes(routes).map(({ path, component }) => (
-          <Route key={path} path={path} element={component} />
-        ))}
-      </Routes>
-    </BrowserRouter>
-  </ThemeProvider>,
+  <OrganizerDataContextProvider>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          {buildRoutesArray(routes).map(({ path, component }) => (
+            <Route key={path} path={path} element={component} />
+          ))}
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  </OrganizerDataContextProvider>,
 );
