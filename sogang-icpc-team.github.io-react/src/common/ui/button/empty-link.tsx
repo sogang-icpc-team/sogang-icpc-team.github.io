334b79d42672067aa86b4387064bb18e35a11f8e
dongzoolee <me@leed.at>
2024-07-04 01:24:10 +0900

    Polish HistoryTab

---
+++ sogang-icpc-team.github.io-react/src/common/ui/button/empty-link.tsx
@@ -0,0 +1,7 @@
+import { Link as RRDLink } from "react-router-dom";
+import styled from "styled-components";
+
+export const EmptyLink = styled(RRDLink)`
+  color: white;
+  text-decoration: none;
+`;
