44059b4e55181587b5c5318285feb5bf20cde674
dongzoolee <me@leed.at>
2024-09-17 01:09:52 +0900

    Update variants on AwardBadge

--- sogang-icpc-team.github.io-react/src/common/ui/award-badge/award-badge.tsx
+++ sogang-icpc-team.github.io-react/src/common/ui/award-badge/award-badge.tsx
@@ -1,14 +1,39 @@
+import { useMemo } from "react";
 import styled from "styled-components";
 
-export type TAwardBadgeVariant = "gold" | "silver" | "bronze";
+export type TAwardBadgeVariant =
+  | "winner"
+  | "gold"
+  | "silver"
+  | "bronze"
+  | "special"
+  | "advanced"
+  | "encouragement";
 
 const _AwardBadge = ({
   className,
+  variant,
 }: {
   className?: string;
   variant: TAwardBadgeVariant;
 }) => {
-  return <span className={className}>⬤</span>;
+  const icon = useMemo(() => {
+    switch (variant) {
+      case "winner":
+        return "★";
+      case "gold":
+      case "silver":
+      case "bronze":
+      case "special":
+        return "⬤";
+      case "advanced":
+        return "▲";
+      case "encouragement":
+        // TODO: 아이콘 결정 필요
+        return "";
+    }
+  }, [variant]);
+  return <span className={className}>{icon}</span>;
 };
 
 export const AwardBadge = styled(_AwardBadge)`
@@ -16,12 +41,18 @@ export const AwardBadge = styled(_AwardBadge)`
   font-size: 13px;
   color: ${({ variant }) => {
     switch (variant) {
+      case "winner":
+        return "#ff8800";
       case "gold":
         return "gold";
       case "silver":
         return "silver";
       case "bronze":
         return "#CD7F32";
+      case "special":
+        return "black";
+      case "advanced":
+        return "#30d10f";
     }
   }};
 `;
