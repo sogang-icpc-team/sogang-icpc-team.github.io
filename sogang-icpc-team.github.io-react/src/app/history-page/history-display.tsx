19131c28956b384ba65556672112a7734362f467
dongzoolee <me@leed.at>
2024-09-16 19:24:07 +0900

    Show contest related links on HistoryDisplay

--- sogang-icpc-team.github.io-react/src/app/history-page/history-display.tsx
+++ sogang-icpc-team.github.io-react/src/app/history-page/history-display.tsx
@@ -2,8 +2,14 @@ import { styled } from "styled-components";
 
 import { Section } from "@ui/section/section";
 import { Table } from "@ui/table/table";
+import { AwardBadge, TAwardBadgeVariant } from "@ui/award-badge/award-badge";
+import { OpenInANewTab } from "@ui/open-in-a-new-tab";
 
 import { useSelectedHistoryContext } from "./contexts/selected-history-context";
+
+const LinksWrapper = styled.ul`
+  margin-top: 16px;
+`;
 
 const _HistoryDisplay = () => {
   const selectedHistory = useSelectedHistoryContext();
@@ -24,15 +30,40 @@ const _HistoryDisplay = () => {
                   </Table.Row>
                 </Table.Header>
                 <Table.Body>
-                  {data.map((r, i) => (
-                    <Table.Row key={i}>
-                      {r.map((c, i) => (
-                        <Table.Cell key={i}>{c}</Table.Cell>
-                      ))}
+                  {data.map((r, rowIndex) => (
+                    <Table.Row key={rowIndex}>
+                      {r.map((c, colIndex) => {
+                        // FIXME: data 형식을 string[] -> json으로 일괄 변경해야 함
+                        if (colIndex === 1) {
+                          return (
+                            <Table.Cell key={colIndex}>
+                              {c}
+                              {award[rowIndex] ? (
+                                <AwardBadge
+                                  variant={
+                                    award[rowIndex] as TAwardBadgeVariant
+                                  }
+                                />
+                              ) : null}
+                            </Table.Cell>
+                          );
+                        }
+                        return <Table.Cell key={colIndex}>{c}</Table.Cell>;
+                      })}
                     </Table.Row>
                   ))}
                 </Table.Body>
               </Table>
+              <LinksWrapper>
+                {links?.map((link) => {
+                  return (
+                    <li key={link[1]}>
+                      <OpenInANewTab href={link[1]}>{link[0]}</OpenInANewTab>
+                    </li>
+                  );
+                })}
+                {/* TODO: Show Review Table */}
+              </LinksWrapper>
             </Section.Body>
           </Section>
         ),
