062ba23c6a5c26fbfd97df8ab52dbc3c9cd3e00a
dongzoolee <me@leed.at>
2024-09-16 22:40:00 +0900

    Refactor to match RRD v6 convention

--- sogang-icpc-team.github.io-react/src/index.tsx
+++ sogang-icpc-team.github.io-react/src/index.tsx
@@ -1,14 +1,20 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
 import { OrganizerDataContextProvider } from "./contexts/organizer-data-context";
 import { buildRoutesArray, routes } from "./routes/routes";
 import { theme } from "./common/themes/theme";
 import { HistoryDataContextProvider } from "./contexts/history-data-context";
-import { ScrollTopOnRouteChange } from "./routes/scroll-top-on-route-change";
 
 import "./common/themes/global.scss";
+
+const router = createBrowserRouter(
+  buildRoutesArray(routes).map(({ path, component }) => ({
+    path,
+    element: component,
+  })),
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
@@ -17,15 +23,7 @@ root.render(
   <HistoryDataContextProvider>
     <OrganizerDataContextProvider>
       <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <ScrollTopOnRouteChange>
-            <Routes>
-              {buildRoutesArray(routes).map(({ path, component }) => (
-                <Route key={path} path={path} element={component} />
-              ))}
-            </Routes>
-          </ScrollTopOnRouteChange>
-        </BrowserRouter>
+        <RouterProvider router={router} />
       </ThemeProvider>
     </OrganizerDataContextProvider>
   </HistoryDataContextProvider>,
